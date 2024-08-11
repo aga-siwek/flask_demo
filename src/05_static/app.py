b9aec3e9e7a19dd96882f4da568de26b88da6ebd
Aga Siwek <siwek.aga1990@gmail.com>
2024-08-11 20:41:11 +0200

    add comments

--- src/05_static/app.py
+++ src/05_static/app.py
@@ -3,9 +3,11 @@ from flask import send_file
 from flask import render_template
 import config
 
+# create flask application
 app = Flask(__name__)
 
 
+# create HTTP routes for our application
 @app.route("/")
 def index():
     return send_file("templates/index.html")
@@ -18,8 +20,10 @@ def about():
 
 @app.route("/contact/")
 def contact():
+    # phone number is added to HTML template
     return render_template("contact.html", phone_number=config.PHONE_NUMBER)
 
 
 if __name__ == "__main__":
+    # start our application
     app.run(port=1234, host="0.0.0.0")
